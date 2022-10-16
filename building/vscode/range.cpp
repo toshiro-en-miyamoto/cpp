17a605fac514a47fb09ca1cfd3c2c81ec9fe5f7e
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-10-17 00:02:23 +0800

    build with vscode finished

---
+++ building/vscode/range.cpp
@@ -0,0 +1,14 @@
+#include <ranges>
+#include <iostream>
+
+int main()
+{
+    auto const ints = {0,1,2,3,4,5};
+    auto even = [](int i) { return 0 == i % 2; };
+    auto square = [](int i) { return i * i; };
+
+    for (int i : ints | std::views::filter(even) | std::views::transform(square)) {
+        std::cout << i << ' ';
+    }
+    std::cout << std::endl;
+}
