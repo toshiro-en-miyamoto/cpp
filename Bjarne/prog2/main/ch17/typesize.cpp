efec2aa3375138e5fdfb21f19db094fba219db7f
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-11-07 22:30:23 +0900

    abbreviated function templates

--- Bjarne/prog2/main/ch17/typesize.cpp
+++ Bjarne/prog2/main/ch17/typesize.cpp
@@ -8,11 +8,23 @@ size_pair typesize(T val)
     return std::make_pair(sizeof(T), sizeof(val));
 }
 
-template size_pair typesize <char> (char);
-template size_pair typesize <int>  (int);
-template size_pair typesize <long> (long);
-template size_pair typesize <int*> (int*);
-template size_pair typesize <bool> (bool);
-template size_pair typesize <double> (double);
+template size_pair typesize(char);
+template size_pair typesize(int);
+template size_pair typesize(long);
+template size_pair typesize(int*);
+template size_pair typesize(bool);
+template size_pair typesize(double);
+
+size_t typesize2(auto val)
+{
+    return sizeof(decltype(val));
+}
+
+template size_t typesize2(char);
+template size_t typesize2(int);
+template size_t typesize2(long);
+template size_t typesize2(int*);
+template size_t typesize2(bool);
+template size_t typesize2(double);
 
 }
--- Bjarne/prog2/main/include/ch17/typesize.h
+++ Bjarne/prog2/main/include/ch17/typesize.h
@@ -16,4 +16,12 @@ extern template size_pair typesize <int*> (int*);
 extern template size_pair typesize <bool> (bool);
 extern template size_pair typesize <double> (double);
 
+size_t typesize2(auto val);
+
+extern template size_t typesize2(char);
+extern template size_t typesize2(int);
+extern template size_t typesize2(long);
+extern template size_t typesize2(int*);
+extern template size_t typesize2(bool);
+extern template size_t typesize2(double);
 }
