8f3dc5f1f3ad22ec82e7abc49bcf1a09a111a9ce
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-11-16 19:03:22 +0900

    ch19 vector w alloc

--- Bjarne/prog2/main/include/ch17/typesize.h
+++ Bjarne/prog2/main/include/ch17/typesize.h
@@ -9,12 +9,12 @@ using size_pair = std::pair<std::size_t, std::size_t>;
 
 template<typename T> size_pair typesize(T val);
 
-extern template size_pair typesize <char> (char);
-extern template size_pair typesize <int>  (int);
-extern template size_pair typesize <long> (long);
-extern template size_pair typesize <int*> (int*);
-extern template size_pair typesize <bool> (bool);
-extern template size_pair typesize <double> (double);
+extern template size_pair typesize(char);
+extern template size_pair typesize(int);
+extern template size_pair typesize(long);
+extern template size_pair typesize(int*);
+extern template size_pair typesize(bool);
+extern template size_pair typesize(double);
 
 size_t typesize2(auto val);
 
--- Bjarne/prog2/test/sniff.cpp
+++ Bjarne/prog2/test/sniff.cpp
@@ -1,2 +1,25 @@
+#include <ch19/myvector.h>
+#include <iostream>
+
+class S {
+    inline static int n{1};
+    int m{};
+    void pre() const { std::cout << "#" << m << std::string(m, ' '); }
+public:
+    S(int x) : m{n++} { pre(); std::cout << "S::S(" << x << ");\n"; }
+    ~S() { pre(); std::cout << "S::~S();\n"; }
+    void id() const { pre(); std::cout << "S::id();\n"; }
+};
+
 int main()
-{}
+{
+    my::vector<S> v1{S{1}};
+    my::vector<S> v3{S{1}, S{2}, S{3}};
+    for (auto i = 0; i < v3.size(); ++i) {
+        v3[i].id();
+    }
+    v3 = v1;
+    for (auto i = 0; i < v3.size(); ++i) {
+        v3[i].id();
+    }
+}
