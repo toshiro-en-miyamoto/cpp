d2009be291a953cb2b946b28a45fba60aec0968e
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-11-23 21:43:20 +0900

    vector currupts memory

--- Bjarne/prog2/main/include/ch19/vector.h
+++ Bjarne/prog2/main/include/ch19/vector.h
@@ -2,9 +2,10 @@
 #include <initializer_list>
 #include <memory>
 #include <utility>
+#include <algorithm>
 #include <iostream>
 
-namespace ch19 {
+namespace ch19b {
 
 template<typename T, typename A>
 struct vector_base
@@ -195,7 +196,6 @@ void vector<T,A>::reserve(std::size_t new_space)
     std::destroy_n(this->elem, this->sz);
     this->sz = 0;
     this->space = 0;
-
     std::swap<vector_base<T,A>>(*this, base);
 }
 
@@ -221,3 +221,177 @@ void vector<T,A>::push_back(const T& val)
 }
 
 }
+
+namespace ch19u
+{
+
+template<typename T>
+void deleter(T arr[])
+{
+    std::cout << "del " << arr << '\n';
+    delete[] arr;
+}
+
+template<typename T>
+class vector
+{
+    std::unique_ptr<T[], void(*)(T[])> ptr;
+    std::size_t sz;
+    std::size_t space;
+public:
+    vector();
+    ~vector() {}
+    explicit vector(std::size_t, T = T());
+    vector(std::initializer_list<T>);
+
+    vector(const vector&);
+    vector& operator=(const vector&);
+
+    vector(vector&&);
+    vector& operator=(vector&&);
+
+    T& operator[](std::size_t);
+    const T& operator[](std::size_t) const;
+    T& at(std::size_t);
+    const T& at(std::size_t) const;
+
+    std::size_t size() const { return sz; }
+    std::size_t capacity() const { return space; }
+
+    void reserve(std::size_t);
+    void resize(std::size_t, T = T());
+    void push_back(const T&);
+};
+
+template<typename T>
+vector<T>::vector()
+    : ptr{nullptr, &deleter}, sz{0}, space{0}
+{}
+
+template<typename T>
+vector<T>::vector(std::size_t size, T def)
+    : ptr{new T[size], &deleter}, sz{size}, space{size}
+{
+    std::cout << "new " << ptr << '\n';
+    std::fill_n(ptr.get(), size, def);
+}
+
+template<typename T>
+vector<T>::vector(std::initializer_list<T> lst)
+    : ptr{new T[lst.size()], &deleter}, sz{lst.size()}, space{lst.size()}
+{
+    std::cout << "new " << ptr << '\n';
+    std::copy(lst.begin(), lst.end(), ptr.get());
+}
+
+template<typename T>
+vector<T>::vector(const vector& v)
+    : ptr{new T[v.sz], &deleter}, sz{v.sz}, space{v.sz}
+{
+    std::cout << "new " << ptr << '\n';
+    std::copy_n(v.ptr.get(), v.size(), ptr.get());
+}
+
+template<typename T>
+vector<T>& vector<T>::operator=(const vector& v)
+{
+    if (this == &v) return *this;
+
+    if (v.sz <= space) {
+        std::destroy_n(ptr.get(), sz);
+        std::copy_n(v.ptr.get(), v.sz, ptr.get());
+    } else {
+        auto p = std::unique_ptr<T[], void(*)(T[])>(new T[v.sz], &deleter);
+        std::cout << "new " << p << '\n';
+
+        std::copy_n(v.ptr.get(), v.sz, p.get());
+        std::destroy_n(ptr.get(), sz);
+        std::swap(p, ptr);
+        space = v.sz;
+    }
+    sz = v.sz;
+    return *this;
+}
+
+template<typename T>
+vector<T>::vector(vector&& v)
+    : ptr{std::exchange(v.ptr, nullptr)}
+    , sz{std::exchange(v.sz, 0)}
+    , space{std::exchange(v.space, 0)}
+{
+    std::cout << "mov " << ptr << '\n';
+}
+
+template<typename T>
+vector<T>& vector<T>::operator=(vector&& v)
+{
+    if (this == &v) return *this;
+
+    ptr = std::exchange(v.ptr, nullptr);
+    sz = std::exchange(v.sz, 0);
+    space = std::exchange(v.space, 0);
+
+    return *this;
+}
+
+template<typename T>
+T& vector<T>::operator[](std::size_t n)
+{
+    return ptr[n];
+}
+
+template<typename T>
+const T& vector<T>::operator[](std::size_t n) const
+{
+    return ptr[n];
+}
+
+template<typename T>
+T& vector<T>::at(std::size_t n)
+{
+    if (n < 0 || sz <= n) throw std::out_of_range();
+    return ptr[n];
+}
+
+template<typename T>
+const T& vector<T>::at(std::size_t n) const
+{
+    if (n < 0 || sz <= n) throw std::out_of_range();
+    return ptr[n];
+}
+
+template<typename T>
+void vector<T>::reserve(std::size_t new_space)
+{
+    if (new_space <= space) return;
+
+    auto p = std::unique_ptr<T[], void(*)(T[])>(new T[new_space], &deleter);
+    std::cout << "new " << p << '\n';
+    std::copy_n(ptr.get(), sz, p.get());
+
+    std::destroy_n(ptr.get(), sz);
+    space = new_space;
+    std::swap(p, ptr);
+}
+
+template<typename T>
+void vector<T>::resize(std::size_t new_sz, T def)
+{
+    reserve(new_sz);
+    std::fill(ptr.get() + sz, ptr.get() + new_sz, def);
+    std::destroy(ptr.get() + new_sz, ptr.get() + sz);
+    sz = new_sz;
+}
+
+template<typename T>
+void vector<T>::push_back(const T& val)
+{
+    if (space == 0) reserve(8);
+    else if (sz == space) reserve(2 * space);
+
+    std::fill_n(ptr.get() + sz, 1, val);
+    ++sz;
+}
+
+}
+
--- Bjarne/prog2/test/ch19/vector_ut.cpp
+++ Bjarne/prog2/test/ch19/vector_ut.cpp
@@ -12,39 +12,185 @@ public:
     void id() const { pre(); std::cout << "S::id();\n"; }
 };
 
-ch19::vector<int> make_vec()
+ch19b::vector<int> make_vec()
 {
-    return ch19::vector<int>(6);
+    return ch19b::vector<int>(6);
 }
 
 int main()
 {
     using namespace boost::ut;
+    using namespace boost::ut::bdd;
 
-"vector_base"_test = [] {
-    ch19::vector<int> v1;
-    ch19::vector<int> v2(3);
-    ch19::vector<int> v3{3};
-    ch19::vector<int> v4{v2};
-    v3 = v2;
-    ch19::vector<int> v5{std::move(make_vec())};
-    v5 = std::move(v4);
-    v5.reserve(13);
-    v5.resize(76);
-    for (auto i = 0; i < 102; ++i) v5.push_back(i);
-};
+    "vector_base"_test = [] {
+        ch19b::vector<int> v1;
+        ch19b::vector<int> v2(3);
+        ch19b::vector<int> v3{3};
+        ch19b::vector<int> v4{v2};
+        v3 = v2;
+        ch19b::vector<int> v5{std::move(make_vec())};
+        v5 = std::move(v4);
+        v5.reserve(13);
+        v5.resize(76);
+//        for (auto i = 0; i < 102; ++i) v5.push_back(i);
+    };
 
-"vector_struct"_test = [] {
-    ch19::vector<S> v2{S{1}, S{2}};
-    ch19::vector<S> v3{S{3}, S{4}, S{5}};
-    for (auto i = 0; i < v3.size(); ++i) {
-        v3[i].id();
-    }
-    v3 = v2;
-    for (auto i = 0; i < v3.size(); ++i) {
-        v3[i].id();
-    }
 
-};
+    "vector_struct"_test = [] {
+        ch19b::vector<S> v2{S{1}, S{2}};
+        ch19b::vector<S> v3{S{3}, S{4}, S{5}};
+        for (auto i = 0; i < v3.size(); ++i) {
+            v3[i].id();
+        }
+        v3 = v2;
+        for (auto i = 0; i < v3.size(); ++i) {
+            v3[i].id();
+        }
+    };
+
+
+    "default constructor"_test = [] {
+        given("I have a default vector") = [] {
+            ch19u::vector<int> v;
+            expect(v.size() == 0_i);
+        };
+    };
+
+    "vector(size, def)"_test = [] {
+        given("I have a vector of 10 elems") = [] {
+            ch19u::vector<int> v(10, 1);
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 10_i);
+            };
+        };
+    };
+
+    "vector(init_list)"_test = [] {
+        given("I have a vector with init list") = [] {
+            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 55_i);
+            };
+        };
+    };
+
+    "copy_constructor"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
+            ch19u::vector<int> v{v1};
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 55_i);
+            };
+        };
+    };
+
+    "copy_assignment"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
+            ch19u::vector<int> v;
+            v = v1;
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 55_i);
+            };
+        };
+    };
+
+    "move constructor"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
+            ch19u::vector<int> v{std::move(v1)};
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 55_i);
+            };
+        };
+    };
+
+    "move assignment"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
+            ch19u::vector<int> v;
+            v = std::move(v1);
+            expect(v.size() == 10_i);
+
+            when("accumlating values") = [&] {
+                int actual = 0;
+                for (auto i = 0; i < 10; ++i) {
+                    actual += v[i];
+                }
+                expect(actual == 55_i);
+            };
+        };
+    };
+
+    "reserve"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
+            expect(v.size() == 10_i);
+
+            when("reserve for 20 elements") = [&] {
+                v.reserve(20);
+                expect(v.size() == 10_i);
+                expect(v.capacity() == 20_i);
+            };
+        };
+    };
+
+    "resize"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
+            expect(v.size() == 10_i);
+
+            when("resize to 12 elements with 1") = [&] {
+                v.resize(12, 1);
+                expect(v.size() == 12_i);
+                expect(v[10] == 1_i);
+                expect(v[11] == 1_i);
+            };
+        };
+    };
+
+    "push_back"_test = [] {
+        given("I have a copy of vector") = [] {
+            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
+            expect(v.size() == 10_i);
+
+            when("push back 11") = [&] {
+                v.push_back(11);
+                expect(v.size() == 11_i);
+                expect(v[10] == 11_i);
+                expect(v.capacity() == 20_i);
+            };
+        };
+    };
 
 }
