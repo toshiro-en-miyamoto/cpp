3fae3553de859c23c8a6f9dbb752b6203efcab75
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-11-27 15:08:44 +0900

    mem for vector ongoing

--- Bjarne/prog2/main/include/ch19/vector.h
+++ Bjarne/prog2/main/include/ch19/vector.h
@@ -1,397 +1,153 @@
+#pragma once
 #include <cstddef>
-#include <initializer_list>
 #include <memory>
 #include <utility>
-#include <algorithm>
+// for debug
 #include <iostream>
 
-namespace ch19b {
+namespace ch19 {
 
-template<typename T, typename A>
-struct vector_base
+/*!
+A memory resource handler.
+Type T must define the default constructor.
+
+        mem& operator=(const mem&);    void reserve(std::size_t);
+
+n < a.n 
+        np = allocate(a.n)
+        uninit_copy_n(a.p, a.n, np)
+        destroy_n(p, n)
+        deallocate(p, n)
+        p = np
+        n = a.n
+
+else
+        destory_n(p, n)                 nop
+        uninit_copy_n(a.p, a.n, p)
+
+*/
+template<typename T>
+class mem
 {
-    A alloc;            // allocator
-    T* elem;            // start of allocation
-    std::size_t sz;     // number of elements
-    std::size_t space;  // amount of allocated space
+    std::allocator<T> alloc;
+    T* p;               // pointer to memory
+    std::size_t n;      // the number of Ts
 
-    vector_base()
-        : alloc{}, elem{nullptr}, sz{0}, space{0}
-    {}
-
-    explicit vector_base(std::size_t n, const A& a = A())
-        : alloc{a}, elem{nullptr}, sz{n}, space{n}
-    {
-        if (0 < n) {
-            elem = alloc.allocate(n);
-            std::cout
-            << "new  (" << elem
-            << ", " << sz
-            << ", " << space
-            << ")\n";
-        }
-    }
-
-    ~vector_base()
-    {
-        if (elem) {
-            alloc.deallocate(elem, space);
-            std::cout
-            << "gone (" << elem
-            << ", " << sz
-            << ", " << space
-            << ")\n";
-        }
-    }
-
-    vector_base(vector_base&& b)
-        : alloc{b.alloc}, elem{b.elem}
-        , sz{b.sz}, space{b.space}
-    {
-        b.elem = nullptr;
-        b.sz = 0;
-        b.space = 0;
-    }
-
-    vector_base& operator=(vector_base&& b)
-    {
-        if (elem) alloc.deallocate(elem, space);
-        elem  = std::exchange(b.elem,  nullptr);
-        sz    = std::exchange(b.sz,    0);
-        space = std::exchange(b.space, 0);
-        return *this;
-    }
-};
-
-template<typename T, typename A = std::allocator<T>>
-class vector : private vector_base<T,A>
-{
+    mem(T* p, std::size_t);
 public:
-    vector();
-    ~vector();
-    explicit vector(std::size_t, T = T());
-    vector(std::initializer_list<T>);
+    mem();
+    ~mem();
+    explicit mem(std::size_t);
+    mem(const mem&);
+    mem& operator=(const mem&);
+    mem(mem&&);
+    mem& operator=(mem&&);
 
-    vector(const vector&);
-    vector& operator=(const vector&);
-
-    vector(vector&&);
-    vector& operator=(vector&&);
-
-    T& operator[](std::size_t);
-    const T& operator[](std::size_t) const;
-    T& at(std::size_t);
-    const T& at(std::size_t) const;
-
-    std::size_t size() const { return this->sz; }
-    std::size_t capacity() const { return this->space; }
+    T* elem() { return p; }
+    const T* elem() const { return p; }
 
     void reserve(std::size_t);
-    void resize(std::size_t, T = T());
-    void push_back(const T&);
-};
-
-template<typename T, typename A>
-vector<T,A>::vector()
-    : vector_base<T,A>{0}
-{}
-
-template<typename T, typename A>
-vector<T,A>::~vector()
-{}
-
-template<typename T, typename A>
-vector<T,A>::vector(std::size_t size, T def)
-    : vector_base<T,A>{size}
-{
-    std::uninitialized_fill_n(this->elem, size, def);
-}
-
-template<typename T, typename A>
-vector<T,A>::vector(std::initializer_list<T> lst)
-    : vector_base<T,A>{lst.size()}
-{
-    std::uninitialized_copy_n(lst.begin(), lst.size(), this->elem);
-}
-
-template<typename T, typename A>
-vector<T,A>::vector(const vector<T,A>& v)
-    : vector_base<T,A>{v.sz}
-{
-    std::uninitialized_copy_n(v.elem, v.sz, this->elem);
-}
-
-template<typename T, typename A>
-vector<T,A>& vector<T,A>::operator=(const vector<T,A>& v)
-{
-    if (this == &v) return *this;
-
-    if (v.sz <= this->space) {
-        std::destroy_n(this->elem, this->sz);
-        std::uninitialized_copy_n(v.elem, v.sz, this->elem);
-        this->sz = v.sz;
-    } else {
-        vector_base<T,A> base{v.sz};
-        std::uninitialized_copy_n(v.elem, v.sz, base.elem);
-        std::destroy_n(this->elem, this->sz);
-        std::swap<vector_base<T,A>>(*this, base);
-    }
-    return *this;
-}
-
-template<typename T, typename A>
-vector<T,A>::vector(vector<T,A>&& v)
-    : vector_base<T,A>{0}
-{
-    std::swap<vector_base<T,A>>(*this, v);
-}
-
-template<typename T, typename A>
-vector<T,A>& vector<T,A>::operator=(vector<T,A>&& v)
-{
-    if (this == &v) return *this;
-
-    std::destroy_n(this->elem, this->sz);
-    this->sz = 0;
-    this->space = 0;
-
-    std::swap<vector_base<T,A>>(*this, v);
-    return *this;
-}
-
-template<typename T, typename A>
-T& vector<T,A>::operator[](std::size_t n)
-{
-    return this->elem[n];
-}
-
-template<typename T, typename A>
-const T& vector<T,A>::operator[](std::size_t n) const
-{
-    return this->elem[n];
-}
-
-template<typename T, typename A>
-T& vector<T,A>::at(std::size_t n)
-{
-    if (n < 0 || this->sz <= n) throw std::out_of_range();
-    return this->elem[n];
-}
-
-template<typename T, typename A>
-const T& vector<T,A>::at(std::size_t n) const
-{
-    if (n < 0 || this->sz <= n) throw std::out_of_range();
-    return this->elem[n];
-}
-
-template<typename T, typename A>
-void vector<T,A>::reserve(std::size_t new_space)
-{
-    if (new_space <= this->space) return;
-
-    vector_base<T,A> base{new_space};
-    std::uninitialized_copy_n(this->elem, this->sz, base.elem);
-
-    std::destroy_n(this->elem, this->sz);
-    this->sz = 0;
-    this->space = 0;
-    std::swap<vector_base<T,A>>(*this, base);
-}
-
-template<typename T, typename A>
-void vector<T,A>::resize(std::size_t new_sz, T def)
-{
-    reserve(new_sz);
-    std::uninitialized_fill(
-        this->elem + this->sz, this->elem + new_sz, def
-    );
-    std::destroy(this->elem + new_sz, this->elem + this->sz);
-    this->sz = new_sz;
-}
-
-template<typename T, typename A>
-void vector<T,A>::push_back(const T& val)
-{
-    if (this->space == 0) reserve(8);
-    else if (this->sz == this->space) reserve(2 * this->space);
-
-    std::uninitialized_fill_n(this->elem + this->sz, 1, val);
-    ++(this->sz);
-}
-
-}
-
-namespace ch19u
-{
-
-template<typename T>
-void deleter(T arr[])
-{
-    std::cout << "del " << arr << '\n';
-    delete[] arr;
-}
-
-template<typename T>
-class vector
-{
-    std::unique_ptr<T[], void(*)(T[])> ptr;
-    std::size_t sz;
-    std::size_t space;
-public:
-    vector();
-    ~vector() {}
-    explicit vector(std::size_t, T = T());
-    vector(std::initializer_list<T>);
-
-    vector(const vector&);
-    vector& operator=(const vector&);
-
-    vector(vector&&);
-    vector& operator=(vector&&);
-
-    T& operator[](std::size_t);
-    const T& operator[](std::size_t) const;
-    T& at(std::size_t);
-    const T& at(std::size_t) const;
-
-    std::size_t size() const { return sz; }
-    std::size_t capacity() const { return space; }
-
-    void reserve(std::size_t);
-    void resize(std::size_t, T = T());
-    void push_back(const T&);
+    std::size_t capacity() const { return n; }
 };
 
 template<typename T>
-vector<T>::vector()
-    : ptr{nullptr, &deleter}, sz{0}, space{0}
-{}
-
-template<typename T>
-vector<T>::vector(std::size_t size, T def)
-    : ptr{new T[size], &deleter}, sz{size}, space{size}
+mem<T>::mem(T* p, std::size_t n)
+    : alloc {}, p {p}, n {n}
 {
-    std::cout << "new " << ptr << '\n';
-    std::fill_n(ptr.get(), size, def);
+    std::cout << "new  " << n << '\n';
+}
+ 
+template<typename T>
+mem<T>::mem()
+    : alloc {}, p {nullptr}, n {0}
+{
+    std::cout << "new  0\n";
 }
 
 template<typename T>
-vector<T>::vector(std::initializer_list<T> lst)
-    : ptr{new T[lst.size()], &deleter}, sz{lst.size()}, space{lst.size()}
+mem<T>::~mem()
 {
-    std::cout << "new " << ptr << '\n';
-    std::copy(lst.begin(), lst.end(), ptr.get());
+    std::cout << "del  " << n << '\n';
+    alloc.deallocate(p, n);
 }
 
 template<typename T>
-vector<T>::vector(const vector& v)
-    : ptr{new T[v.sz], &deleter}, sz{v.sz}, space{v.sz}
+mem<T>::mem(std::size_t n)
+    : alloc {}, p {nullptr}, n {n}
 {
-    std::cout << "new " << ptr << '\n';
-    std::copy_n(v.ptr.get(), v.size(), ptr.get());
+    std::cout << "size " << n << '\n';
+    p = alloc.allocate(n);
+    std::uninitialized_fill_n(p, n, T {});
 }
 
 template<typename T>
-vector<T>& vector<T>::operator=(const vector& v)
+mem<T>::mem(const mem<T>& that)
+    : alloc {}, p {nullptr}, n {that.n}
 {
-    if (this == &v) return *this;
+    p = alloc.allocate(n);
+    std::uninitialized_copy_n(that.p, n, p);
+}
 
-    if (v.sz <= space) {
-        std::destroy_n(ptr.get(), sz);
-        std::copy_n(v.ptr.get(), v.sz, ptr.get());
+template<typename T>
+mem<T>& mem<T>::operator=(const mem<T>& that)
+{
+    if (this == &that) return *this;
+
+    if (n < that.n) {
+        T* new_p = alloc.allocate(that.n);
+        std::uninitialized_copy_n(p, n, new_p);
+
+        std::destroy_n(p, n);
+        alloc.deallocate(p, n);
+
+        p = new_p;
+        n = that.n;
     } else {
-        auto p = std::unique_ptr<T[], void(*)(T[])>(new T[v.sz], &deleter);
-        std::cout << "new " << p << '\n';
-
-        std::copy_n(v.ptr.get(), v.sz, p.get());
-        std::destroy_n(ptr.get(), sz);
-        std::swap(p, ptr);
-        space = v.sz;
+        std::destroy_n(p, n);
+        std::uninitialized_copy_n(that.p, that.n, p);
     }
-    sz = v.sz;
-    return *this;
-}
-
-template<typename T>
-vector<T>::vector(vector&& v)
-    : ptr{std::exchange(v.ptr, nullptr)}
-    , sz{std::exchange(v.sz, 0)}
-    , space{std::exchange(v.space, 0)}
-{
-    std::cout << "mov " << ptr << '\n';
-}
-
-template<typename T>
-vector<T>& vector<T>::operator=(vector&& v)
-{
-    if (this == &v) return *this;
-
-    ptr = std::exchange(v.ptr, nullptr);
-    sz = std::exchange(v.sz, 0);
-    space = std::exchange(v.space, 0);
 
     return *this;
 }
 
 template<typename T>
-T& vector<T>::operator[](std::size_t n)
+mem<T>::mem(mem<T>&& that)
+    : alloc {}, p {that.p}, n {that.n}
 {
-    return ptr[n];
+    std::cout << "mtor " << n << '\n';
+    that.p = nullptr;
+    that.n = 0;
 }
 
 template<typename T>
-const T& vector<T>::operator[](std::size_t n) const
+mem<T>& mem<T>::operator=(mem<T>&& that)
 {
-    return ptr[n];
+    std::cout << "move " << that.n << '\n';
+    if (this == &that) return *this;
+
+    // std::destroy_n(p, n);
+    p = std::exchange(that.p, nullptr);
+    n = std::exchange(that.n, 0);
+
+    return *this;
 }
 
 template<typename T>
-T& vector<T>::at(std::size_t n)
+void mem<T>::reserve(std::size_t new_n)
 {
-    if (n < 0 || sz <= n) throw std::out_of_range();
-    return ptr[n];
+    if (n < new_n) {
+        std::cout << "reserving from " << n
+        << " to " << new_n
+        << '\n';
+
+        T* new_p = alloc.allocate(new_n);
+        std::uninitialized_copy_n(p, n, new_p);
+        mem<T> new_mem {new_p, new_n};
+
+        std::destroy_n(p, n);
+        std::swap(new_mem, *this);
+    }
+    std::cout << "reserved\n";
 }
 
-template<typename T>
-const T& vector<T>::at(std::size_t n) const
-{
-    if (n < 0 || sz <= n) throw std::out_of_range();
-    return ptr[n];
-}
-
-template<typename T>
-void vector<T>::reserve(std::size_t new_space)
-{
-    if (new_space <= space) return;
-
-    auto p = std::unique_ptr<T[], void(*)(T[])>(new T[new_space], &deleter);
-    std::cout << "new " << p << '\n';
-    std::copy_n(ptr.get(), sz, p.get());
-
-    std::destroy_n(ptr.get(), sz);
-    space = new_space;
-    std::swap(p, ptr);
-}
-
-template<typename T>
-void vector<T>::resize(std::size_t new_sz, T def)
-{
-    reserve(new_sz);
-    std::fill(ptr.get() + sz, ptr.get() + new_sz, def);
-    std::destroy(ptr.get() + new_sz, ptr.get() + sz);
-    sz = new_sz;
-}
-
-template<typename T>
-void vector<T>::push_back(const T& val)
-{
-    if (space == 0) reserve(8);
-    else if (sz == space) reserve(2 * space);
-
-    std::fill_n(ptr.get() + sz, 1, val);
-    ++sz;
-}
-
-}
-
+}   // end of namespace ch19
--- Bjarne/prog2/test/ch19/vector_ut.cpp
+++ Bjarne/prog2/test/ch19/vector_ut.cpp
@@ -1,196 +1,69 @@
 #include <ch19/vector.h>
 #include <boost/ut.hpp>
 #include <iostream>
+#include <sstream>
 
-class S {
-    inline static int n{1};
-    int m{};
-    void pre() const { std::cout << "#" << m << std::string(m, ' '); }
-public:
-    S(int x) : m{n++} { pre(); std::cout << "S::S(" << x << ");\n"; }
-    ~S() { pre(); std::cout << "S::~S();\n"; }
-    void id() const { pre(); std::cout << "S::id();\n"; }
-};
-
-ch19b::vector<int> make_vec()
+struct int_char
 {
-    return ch19b::vector<int>(6);
-}
+    int n;
+    char c;
+};
 
 int main()
 {
     using namespace boost::ut;
     using namespace boost::ut::bdd;
 
-    "vector_base"_test = [] {
-        ch19b::vector<int> v1;
-        ch19b::vector<int> v2(3);
-        ch19b::vector<int> v3{3};
-        ch19b::vector<int> v4{v2};
-        v3 = v2;
-        ch19b::vector<int> v5{std::move(make_vec())};
-        v5 = std::move(v4);
-        v5.reserve(13);
-        v5.resize(76);
-//        for (auto i = 0; i < 102; ++i) v5.push_back(i);
-    };
+    suite mem_test = [] {
+        "default constructor"_test = [] {
+            given("a default mem") = [] {
+                std::ostringstream local;
+                auto cout_buff = std::cout.rdbuf();
+                std::cout.rdbuf(local.rdbuf());
 
+                when("is constructed") = [] {
+                    ch19::mem<int_char> m;
+                    expect(m.elem() == nullptr);
+                    expect(m.capacity() == 0_ul);
+                };
 
-    "vector_struct"_test = [] {
-        ch19b::vector<S> v2{S{1}, S{2}};
-        ch19b::vector<S> v3{S{3}, S{4}, S{5}};
-        for (auto i = 0; i < v3.size(); ++i) {
-            v3[i].id();
-        }
-        v3 = v2;
-        for (auto i = 0; i < v3.size(); ++i) {
-            v3[i].id();
-        }
-    };
-
-
-    "default constructor"_test = [] {
-        given("I have a default vector") = [] {
-            ch19u::vector<int> v;
-            expect(v.size() == 0_i);
+                std::cout.rdbuf(cout_buff);
+                expect(local.str() ==
+                    "new  0\n"
+                    "del  0\n")
+                << local.str();
+            };
         };
-    };
+        "reserve"_test = [] {
+            given("a default mem") = [] {
+                std::ostringstream local;
+                auto cout_buff = std::cout.rdbuf();
+                std::cout.rdbuf(local.rdbuf());
 
-    "vector(size, def)"_test = [] {
-        given("I have a vector of 10 elems") = [] {
-            ch19u::vector<int> v(10, 1);
-            expect(v.size() == 10_i);
+                when("reserves three elememts") = [] {
+                    ch19::mem<int_char> m;
+                    m.reserve(3);
+                    expect(m.elem() != nullptr);
+                    expect(m.capacity() == 3_ul);
+                };
 
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 10_i);
+                std::cout.rdbuf(cout_buff);
+                expect(local.str() ==
+                    "new  0\n"
+                    "reserving from 0 to 3\n"
+                    "new  3\n"  // mem new_mem {new_p, 3}
+                                // swap(new_mem, *this)
+                    "mtor 3\n"  //      tmp {move(new_mem)}
+                    "move 0\n"  //      new_mem = move(*this)
+                    "move 3\n"  //      *this = move(tmp)
+                    "del  0\n"  //      deleting tmp
+                    "del  0\n"  // deleting new_mem (formally *this)
+                    "reserved\n"
+                    "del  3\n") // deleting m (oramlly new_mem)
+                << local.str();
             };
         };
     };
 
-    "vector(init_list)"_test = [] {
-        given("I have a vector with init list") = [] {
-            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
-            expect(v.size() == 10_i);
-
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 55_i);
-            };
-        };
-    };
-
-    "copy_constructor"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
-            ch19u::vector<int> v{v1};
-            expect(v.size() == 10_i);
-
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 55_i);
-            };
-        };
-    };
-
-    "copy_assignment"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
-            ch19u::vector<int> v;
-            v = v1;
-            expect(v.size() == 10_i);
-
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 55_i);
-            };
-        };
-    };
-
-    "move constructor"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
-            ch19u::vector<int> v{std::move(v1)};
-            expect(v.size() == 10_i);
-
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 55_i);
-            };
-        };
-    };
-
-    "move assignment"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v1{1,2,3,4,5,6,7,8,9,10};
-            ch19u::vector<int> v;
-            v = std::move(v1);
-            expect(v.size() == 10_i);
-
-            when("accumlating values") = [&] {
-                int actual = 0;
-                for (auto i = 0; i < 10; ++i) {
-                    actual += v[i];
-                }
-                expect(actual == 55_i);
-            };
-        };
-    };
-
-    "reserve"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
-            expect(v.size() == 10_i);
-
-            when("reserve for 20 elements") = [&] {
-                v.reserve(20);
-                expect(v.size() == 10_i);
-                expect(v.capacity() == 20_i);
-            };
-        };
-    };
-
-    "resize"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
-            expect(v.size() == 10_i);
-
-            when("resize to 12 elements with 1") = [&] {
-                v.resize(12, 1);
-                expect(v.size() == 12_i);
-                expect(v[10] == 1_i);
-                expect(v[11] == 1_i);
-            };
-        };
-    };
-
-    "push_back"_test = [] {
-        given("I have a copy of vector") = [] {
-            ch19u::vector<int> v{1,2,3,4,5,6,7,8,9,10};
-            expect(v.size() == 10_i);
-
-            when("push back 11") = [&] {
-                v.push_back(11);
-                expect(v.size() == 11_i);
-                expect(v[10] == 11_i);
-                expect(v.capacity() == 20_i);
-            };
-        };
-    };
-
+    suite vector_test = [] {};
 }
