c2e86923a0ca172e362b80d0beb33c8219a3a008
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-11-28 22:55:37 +0900

    next is drill ch19

--- Bjarne/prog2/main/include/ch19/vector.h
+++ Bjarne/prog2/main/include/ch19/vector.h
@@ -64,7 +64,7 @@ mem<T>::mem(std::size_t n)
 {
     std::cout << "size " << n << '\n';
     p = alloc.allocate(n);
-    std::uninitialized_fill_n(p, n, T {});
+    // std::uninitialized_fill_n(p, n, T {});
 }
 
 template<typename T>
@@ -86,12 +86,10 @@ mem<T>& mem<T>::operator=(const mem<T>& that)
     << '\n';
 
     if (n < that.n) {
-        T* new_p = alloc.allocate(that.n);
-        std::uninitialized_copy_n(p, n, new_p);
-        mem<T> new_mem {new_p, that.n};
-
+        mem<T> new_mem { alloc.allocate(that.n), that.n };
+        std::uninitialized_copy_n(that.p, that.n, new_mem.p);
         std::destroy_n(p, n);
-        std::swap(new_mem, *this);
+        std::swap<mem<T>>(new_mem, *this);
     } else {
         std::destroy_n(p, n);
         std::uninitialized_copy_n(that.p, that.n, p);
@@ -125,18 +123,16 @@ mem<T>& mem<T>::operator=(mem<T>&& that)
 template<typename T>
 void mem<T>::reserve(std::size_t new_n)
 {
-    if (n < new_n) {
-        std::cout << "resv " << n
-        << ':' << new_n
-        << '\n';
+    if (new_n <= n) return;
 
-        T* new_p = alloc.allocate(new_n);
-        std::uninitialized_copy_n(p, n, new_p);
-        mem<T> new_mem {new_p, new_n};
+    std::cout << "resv " << n
+    << ':' << new_n
+    << '\n';
 
-        std::destroy_n(p, n);
-        std::swap(new_mem, *this);
-    }
+    mem<T> new_mem {alloc.allocate(new_n), new_n};
+    std::uninitialized_copy_n(p, n, new_mem.p);
+    std::destroy_n(p, n);
+    std::swap<mem<T>>(new_mem, *this);
 }
 
 template<typename T>
@@ -146,7 +142,7 @@ class vector : protected mem<T>
 public:
     vector() : sz {0}, mem<T> {} {}
     ~vector() = default;
-    explicit vector(std::size_t n) : sz {n}, mem<T>(n) {}
+    explicit vector(std::size_t, T = T {});
     vector(std::initializer_list<T>);
 
     vector(const vector&) = default;
@@ -160,15 +156,45 @@ public:
     using mem<T>::capacity;
 
     using mem<T>::reserve;
-    void resize(std::size_t);
-    void push_back(T);
+    void resize(std::size_t, T = T {});
+    void push_back(const T&);
 };
 
 template<typename T>
+vector<T>::vector(std::size_t n, T val)
+    : mem<T>(n), sz {n}
+{
+    std::fill_n(this->elem(), n, val);
+}
+
+template<typename T>
 vector<T>::vector(std::initializer_list<T> list)
-    : sz {list.size()}, mem<T>(list.size())
+    : mem<T>(list.size()), sz {list.size()}
 {
     std::copy(list.begin(), list.end(), this->elem());
+}
+
+template<typename T>
+void vector<T>::resize(std::size_t new_sz, T val)
+{
+    reserve(new_sz);
+    if (sz < new_sz) {
+        std::fill_n(this->elem() + sz, new_sz - sz, val);
+    } else if (new_sz < sz) {
+        std::destroy(this->elem() + new_sz, this->elem() + sz);
+    }
+    sz = new_sz;
+}
+
+template<typename T>
+void vector<T>::push_back(const T& val)
+{
+    const auto space = capacity();
+    if (space == 0) reserve(8);
+    else if (sz == space) reserve(2 * space);
+
+    std::fill_n(this->elem() + sz, 1, val);
+    ++sz;
 }
 
 }   // end of namespace ch19
