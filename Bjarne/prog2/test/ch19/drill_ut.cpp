a143823792522d6369e3d305984463f36f6414ac
Toshiro Miyamoto <toshiro.en.miyamoto@gmail.com>
2022-12-02 22:07:18 +0900

    drill and exercise

---
+++ Bjarne/prog2/test/ch19/drill_ut.cpp
@@ -0,0 +1,66 @@
+#include <ch19/drill.h>
+#include <boost/ut.hpp>
+#include <string>
+#include <vector>
+
+int main()
+{
+    using namespace boost::ut;
+    using namespace boost::ut::bdd;
+    using namespace boost::ut::literals;
+
+    "ch19 drill"_test = [] {
+        given("S<int>") = [] {
+            ch19::S<int> s {1};
+            when("access val") = [&] {
+                expect(s.get() == 1_i);
+            };
+            when("assign val") = [&] {
+                s = 2;
+                expect(s.get() == 2_i);
+            };
+        };
+        given("S<char>") = [] {
+            ch19::S<char> s {'a'};
+            when("access val") = [&] {
+                expect(s.get() == 'a');
+            };
+            when("assign val") = [&] {
+                s = 'b';
+                expect(s.get() == 'b');
+            };
+        };
+        given("S<double>") = [] {
+            ch19::S<double> s {1.0};
+            when("access val") = [&] {
+                expect(s.get() == 1.0_d);
+            };
+            when("assign val") = [&] {
+                s = 2.0;
+                expect(s.get() == 2.0_d);
+            };
+        };
+        given("S<std::string>") = [] {
+            ch19::S<std::string> s {"str"};
+            when("access val") = [&] {
+                expect(s.get() == "str");
+            };
+            when("assign val") = [&] {
+                s = "xyz";
+                expect(s.get() == "xyz");
+            };
+        };
+        given("S<std::vector<int>>") = [] {
+            ch19::S<std::vector<int>> s {{1, 2}};
+            when("access val") = [&] {
+                expect(s.get()[0] == 1_i);
+                expect(s.get()[1] == 2_i);
+            };
+            when("assign val") = [&] {
+                s = ch19::S<std::vector<int>>{{3, 4}};
+                expect(s.get()[0] == 3_i);
+                expect(s.get()[1] == 4_i);
+            };
+        };
+    };
+}
