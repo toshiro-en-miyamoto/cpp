f24dfb6333240003ad9006b156185bab90ec44b1
Toshiro Miyamoto <toshiro.en.miyamoto@gmail>
2024-11-05 00:33:17 +0800

    ppp3 ch03 ex15 what is size of range

--- Bjarne/ppp3/ch03/ex15.cpp
+++ Bjarne/ppp3/ch03/ex15.cpp
@@ -3,84 +3,92 @@
   Given a series of positive integers, find the max, min and mode of that series.
   The number that appears the most times in a sequence is called the mode.
 */
+
 #include <array>
-constexpr std::array<double, 20> samples {
-  81, 67, 47, 78, 52, 84, 76, 73, 54, 91, 52, 67, 70, 68, 71, 84, 71, 61, 69, 43
+// the value type must be 'long double' so as to calculate the mean of dataset:
+// Range::size() is unsigned long int
+constexpr std::array<long double, 20> samples {
+  82, 67, 47, 78, 52, 84, 76, 73, 54, 91, 52, 67, 70, 68, 71, 84, 71, 61, 69, 43
 };
+
+#include <type_traits>
+static_assert(std::is_same_v<long double, decltype(samples)::value_type>);
 
 #include <algorithm>
 #include <ranges>
 #include <concepts>
 
-template <std::ranges::input_range R>
-requires std::totally_ordered<std::ranges::range_value_t<R>>
+template <std::ranges::input_range R, class T = std::ranges::range_value_t<R>>
+  requires std::totally_ordered<T>
 [[nodiscard]]
-constexpr std::ranges::range_value_t<R> max_v(R&& r) {
-  std::ranges::range_value_t<R> v = *(r.begin());
+constexpr T max_v(R&& r) {
+  T v = *(r.begin());
   for (auto e : r) {
     if (v < e) v = e;
   }
   return v;
 }
 
-static_assert(std::ranges::max(samples) == 91);
+static_assert(std::ranges::max(samples) == static_cast<decltype(samples)::value_type>(91));
 static_assert(max_v(std::views::all(samples)) == std::ranges::max(samples));
 
-template <std::ranges::input_range R>
-requires std::totally_ordered<std::ranges::range_value_t<R>>
+template <std::ranges::input_range R, class T = std::ranges::range_value_t<R>>
+  requires std::totally_ordered<T>
 [[nodiscard]]
-constexpr std::ranges::range_value_t<R> min_v(R&& r) {
-  std::ranges::range_value_t<R> v = *(r.begin());
+constexpr T min_v(R&& r) {
+  T v = *(r.begin());
   for (auto e : r) {
     if (v > e) v = e;
   }
   return v;
 }
 
-static_assert(std::ranges::min(samples) == 43);
+static_assert(std::ranges::min(samples) == static_cast<decltype(samples)::value_type>(43));
 static_assert(min_v(std::views::all(samples)) == std::ranges::min(samples));
 
-template <std::ranges::input_range R>
+template <std::ranges::input_range R, class T = std::ranges::range_value_t<R>>
+  requires std::totally_ordered<T> and std::floating_point<T>
 [[nodiscard]]
-inline constexpr auto range_v(R&& r) {
-  return std::distance(r.begin(), r.end());
+constexpr T range_v(R&& r) {
+  auto [min, max] = std::ranges::minmax(r);
+  return max - min;
 }
 
-static_assert(samples.size() == 20);
-static_assert(std::views::all(samples).size() == samples.size());
-static_assert(range_v(std::views::all(samples)) == samples.size());
+static_assert(
+  range_v(std::views::all(samples)) == static_cast<decltype(samples)::value_type>(91 - 43)
+);
 
-template <typename T>
-concept dividable = requires (T a, std::size_t b) {
-  a + b;  a - b;  a * b;  a / b;
-};
-
-enum class division_error {
-  div0,  overflow
-};
-
-#include <expected>
-
-template <std::ranges::input_range R>
-requires std::totally_ordered<std::ranges::range_value_t<R>>
-        && dividable<std::ranges::range_value_t<R>>
+template <std::ranges::input_range R, class T = std::ranges::range_value_t<R>>
+  requires std::totally_ordered<T> and std::floating_point<T>
 [[nodiscard]]
-constexpr auto mean_v(R&& r)
-  -> std::expected<std::ranges::range_value_t<R>, division_error> 
-{
-  auto sum
-  = std::ranges::fold_left(r, 0, std::plus<std::ranges::range_value_t<R>>());
-  if (r.size() == 0) {
-    return std::unexpected(division_error::div0);
+inline constexpr T sum_v(R&& r) {
+  return std::ranges::fold_left(r, static_cast<T>(0), std::plus<T>());
+}
+
+
+static_assert(
+  sum_v(std::views::all(samples)) == static_cast<decltype(samples)::value_type>(1360)
+);
+
+#include <stdexcept>
+
+template <std::ranges::input_range R, class T = std::ranges::range_value_t<R>>
+  requires std::totally_ordered<T> and std::floating_point<T>
+[[nodiscard]]
+constexpr T mean_v(R&& r) {
+  auto sum = std::ranges::fold_left(r, static_cast<T>(0), std::plus<T>());
+  if (std::ranges::empty(r)) {
+    // todo: the string literal to be retrieved from a injected resource
+    throw std::logic_error{"dataset must not be empty"};
   } else {
     return sum / r.size();
   }
 }
 
-#include <numeric>  // sum = 1359; mean = 67.95
-constexpr auto samples_sum = std::accumulate(samples.begin(), samples.end(), 0.0);
-static_assert(samples_sum / samples.size() == 67.95);
-static_assert(mean_v(std::views::all(samples)) == 67.95);
+static_assert(
+  mean_v(std::views::all(samples)) == static_cast<decltype(samples)::value_type>(68.0)
+);
 
-int main() {
+int main()
+{
 }
